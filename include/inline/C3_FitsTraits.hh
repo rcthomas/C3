66278ae0e6ff0a37567d3ae76a0568d7c835634f
R. C. Thomas <rcthomas@lbl.gov>
2015-07-22 16:20:03 -0700

    Change here to support datatype and bitpix, may need to check...

--- include/inline/C3_FitsTraits.hh
+++ include/inline/C3_FitsTraits.hh
@@ -4,71 +4,83 @@
 template<>
 struct C3::FitsType< unsigned char >
 {
-    static const int value = TBYTE;
+    static const int datatype = TBYTE;
+    static const int bitpix   = BYTE_IMG;
 };
 
 template<>
 struct C3::FitsType< signed char >
 {
-    static const int value = TSBYTE;
+    static const int datatype = TSBYTE;
+    static const int bitpix   = SBYTE_IMG;
 };
 
 template<>
 struct C3::FitsType< bool >
 {
-    static const int value = TLOGICAL;
+    static const int datatype = TLOGICAL;
+    static const int bitpix   = BYTE_IMG;   // VERIFY
 };
 
 template<>
 struct C3::FitsType< unsigned short int >
 {
-    static const int value = TUSHORT;
+    static const int datatype = TUSHORT;
+    static const int bitpix   = USHORT_IMG;
 };
 
 template<>
 struct C3::FitsType< signed short int >
 {
-    static const int value = TSHORT;
+    static const int datatype = TSHORT;
+    static const int bitpix   = SHORT_IMG;
 };
 
 template<>
 struct C3::FitsType< unsigned int >
 {
-    static const int value = TUINT;
+    static const int datatype = TUINT;
+    static const int bitpix   = ULONG_IMG;
 };
 
 template<>
 struct C3::FitsType< signed int >
 {
-    static const int value = TINT;
+    static const int datatype = TINT;
+    static const int bitpix   = LONG_IMG;
 };
 
 template<>
 struct C3::FitsType< unsigned long int >
 {
-    static const int value = TULONG;
+    static const int datatype = TULONG;
+    static const int bitpix   = ULONG_IMG;
 };
 
 template<>
 struct C3::FitsType< signed long int >
 {
-    static const int value = TLONG;
+    static const int datatype = TLONG;
+    static const int bitpix   = LONG_IMG;
 };
 
 template<>
 struct C3::FitsType< float >
 {
-    static const int value = TFLOAT;
+    static const int datatype = TFLOAT;
+    static const int bitpix   = FLOAT_IMG;
 };
 
 template<>
 struct C3::FitsType< signed long long int >
 {
-    static const int value = TLONGLONG;
+    static const int datatype = TLONGLONG;
+    static const int bitpix   = LONGLONG_IMG;
 };
 
 template<>
 struct C3::FitsType< double >
 {
-    static const int value = TDOUBLE;
+    static const int datatype = TDOUBLE;
+    static const int bitpix   = DOUBLE_IMG;
 };
