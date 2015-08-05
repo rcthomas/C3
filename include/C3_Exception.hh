b6735ee70e2d29bedda924d46c4429f515d5fc9c
R. C. Thomas <rcthomas@lbl.gov>
2015-08-05 16:45:43 -0700

    Simplified calls with variadic templates for handling random argument types.  Not tested yet.

--- include/C3_Exception.hh
+++ include/C3_Exception.hh
@@ -8,15 +8,38 @@ namespace C3
 
     /// @class Exception
     /// @brief Base exception class.
-
-    struct Exception : public std::runtime_error
-    {
-
-        /// Constructor.
-        Exception( const std::string& message ) : std::runtime_error( message ) {}
-
+    
+    class Exception : public std::runtime_error
+    {   
+        
+        public :    // Public methods.
+            
+            /// Named constructor for list of arguments to format into message string.
+            template< class... T > static Exception create( const T&... args );
+            
+            /// Constructor.
+            explicit Exception( const std::string& message ) :
+                std::runtime_error( message ) {}
+        
+        protected : // Protected methods.
+            
+            /// Format list of arguments into message string. 
+            template< class... T > static std::string format( const T&... args );
+        
+        private :   // Private methods.
+            
+            /// Format argument of any type into string stream.
+            template< class T > 
+            static std::stringstream& _format( std::stringstream& stream, const T& arg );
+            
+            /// Recursively format arguments of any type into string stream.
+            template< class T, class... U >
+            static std::stringstream& _format( std::stringstream& stream, const T& head, const U&... tail );
+    
     };
 
 }
+
+#include "inline/C3_Exception.hh"
 
 #endif
---
+++ include/inline/C3_Exception.hh
@@ -0,0 +1,36 @@
+
+// Named constructor for list of arguments to format into message string.
+
+template< class... T >
+inline C3::Exception C3::Exception::create( const T&... args )
+{
+    return C3::Exception( format( args... ) );
+}
+
+// Format list of arguments into message string.
+
+template< class... T >
+inline std::string C3::Exception::format( const T&... args )
+{
+    std::stringstream ss;
+    ss << std::boolalpha;
+    return format( ss, args... ).str();
+}
+
+// Format argument of any type into string stream.
+
+template< class T >
+inline std::stringstream& C3::Exception::_format( std::stringstream& stream, const T& arg )
+{
+    stream << arg;
+    return stream;
+}
+
+// Recursively format arguments of any type into string stream.
+
+template< class T, class... U >
+inline std::stringstream& C3::Exception::_format( std::stringstream& stream, const T& head, const U&... tail )
+{
+    stream << head << " ";
+    return format( stream, tail... );
+}
