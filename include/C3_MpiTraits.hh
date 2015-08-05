ca2bb30e021c72da131903412f0310875ab9657b
R. C. Thomas <rcthomas@lbl.gov>
2015-08-04 17:13:36 -0700

    Type traits for MPI things.

---
+++ include/C3_MpiTraits.hh
@@ -0,0 +1,14 @@
+#ifndef C3_MPI_TRAITS_HH
+#define C3_MPI_TRAITS_HH
+
+namespace C3
+{
+
+    template< class T >
+    struct MpiType {};
+
+}
+
+#include "inline/C3_MpiTraits.hh"
+
+#endif
---
+++ include/inline/C3_MpiTraits.hh
@@ -0,0 +1,74 @@
+
+#include <mpi.h>
+
+template<>
+struct C3::MpiType< unsigned char >
+{
+    static const MPI_Datatype datatype = MPI_UNSIGNED_CHAR;
+};
+
+template<>
+struct C3::MpiType< signed char >
+{
+    static const MPI_Datatype datatype = MPI_SIGNED_CHAR;
+};
+
+//template<>
+//struct C3::MpiType< bool >
+//{
+//    static const MPI_Datatype datatype = MPI_C_BOOL;      /// BOOL?
+//};
+
+template<>
+struct C3::MpiType< unsigned short int >
+{
+    static const MPI_Datatype datatype = MPI_UNSIGNED_SHORT;
+};
+
+template<>
+struct C3::MpiType< signed short int >
+{
+    static const MPI_Datatype datatype = MPI_SHORT;
+};
+
+template<>
+struct C3::MpiType< unsigned int >
+{
+    static const MPI_Datatype datatype = MPI_UNSIGNED;
+};
+
+template<>
+struct C3::MpiType< signed int >
+{
+    static const MPI_Datatype datatype = MPI_INT;
+};
+
+template<>
+struct C3::MpiType< unsigned long int >
+{
+    static const MPI_Datatype datatype = MPI_UNSIGNED_LONG;
+};
+
+template<>
+struct C3::MpiType< signed long int >
+{
+    static const MPI_Datatype datatype = MPI_LONG;
+};
+
+template<>
+struct C3::MpiType< float >
+{
+    static const MPI_Datatype datatype = MPI_FLOAT;
+};
+
+template<>
+struct C3::MpiType< signed long long int >
+{
+    static const MPI_Datatype datatype = MPI_LONG_LONG_INT;
+};
+
+template<>
+struct C3::MpiType< double >
+{
+    static const MPI_Datatype datatype = MPI_DOUBLE;
+};
